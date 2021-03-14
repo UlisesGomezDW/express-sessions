29eb2e3c048bc7546b77775edf024cc56588c466
UlisesGomezDW <ulisesgomez08live@gmail.com>
2021-03-13 23:25:29 -0600

    test/sessions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 const express = require("express")
 const app = express()
+const path = require("path")
+const morgan = require("morgan")
 const port = 4000
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
+const session = require("express-session")
+const flash = require("connect-flash")
+
+// Settings
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+// Middlewares
+app.use(morgan("dev"))
+app.use(express.urlencoded({ extended: false }))
+app.use(
+    session({
+        secret: "mysecretkey",
+        resave: false,
+        saveUninitialized: false,
+    })
+)
+app.use(flash())
+
+app.use((req, res, next) => {
+    app.locals.messages = req.flash("success")
+    next()
 })
+
+app.use(require("./routes/index"))
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
